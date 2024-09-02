1408c3a20b3639c4c23355252c3d7d3a3f87e169
HelenYazh <olenka.yazh05@gmail.com>
2024-09-02 21:25:04 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from "react-router";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import MoviesPage from "./pages/MoviesPage";
+import MovieDetailsPage from "./pages/MovieDetailsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import MovieCast from "./components/MovieCast/MovieCast";
+import MovieReviews from "./components/MovieReviews/MovieReviews";
+import Navigation from "./components/Navigation/Navigation";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navigation />
+      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="cast" element={<MovieCast />} />
+          <Route path="reviews" element={<MovieReviews />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
